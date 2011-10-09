a3942f769901b782fed440873cd5157f9e2e3fb5
David Stolarsky <david.stolarsky@gmail.com>
2011-10-09 02:02:37 -0400

    dumping osc incoming joint pose

--- BodyPuppet/src/BodyPuppetApp.cpp
+++ BodyPuppet/src/BodyPuppetApp.cpp
@@ -16,13 +16,6 @@ void BodyPuppet::setup(){
 
 //--------------------------------------------------------------
 void BodyPuppet::update(){
-	// hide old messages
-	for ( int i=0; i<NUM_MSG_STRINGS; i++ )
-	{
-		if ( timers[i] < ofGetElapsedTimef() )
-			msg_strings[i] = "";
-	}
-
 	// check for waiting messages
 	while( receiver.hasWaitingMessages() )
 	{
@@ -30,47 +23,32 @@ void BodyPuppet::update(){
 		ofxOscMessage m;
 		receiver.getNextMessage( &m );
 
-		// check for mouse moved message
-		if ( m.getAddress() == "/mouse/position" )
-		{
-			// both the arguments are int32's
-			mouseX = m.getArgAsInt32( 0 );
-			mouseY = m.getArgAsInt32( 1 );
-		}
-		// check for mouse button message
-		else if ( m.getAddress() == "/mouse/button" )
-		{
-			// the single argument is a string
-			mouseButtonState = m.getArgAsString( 0 ) ;
-		}
-		else
-		{
-			// unrecognized message: display on the bottom of the screen
-			string msg_string;
-			msg_string = m.getAddress();
-			msg_string += ": ";
-			for ( int i=0; i<m.getNumArgs(); i++ )
-			{
-				// get the argument type
-				msg_string += m.getArgTypeName( i );
-				msg_string += ":";
-				// display the argument - make sure we get the right type
-				if( m.getArgType( i ) == OFXOSC_TYPE_INT32 )
-					msg_string += ofToString( m.getArgAsInt32( i ) );
-				else if( m.getArgType( i ) == OFXOSC_TYPE_FLOAT )
-					msg_string += ofToString( m.getArgAsFloat( i ) );
-				else if( m.getArgType( i ) == OFXOSC_TYPE_STRING )
-					msg_string += m.getArgAsString( i );
-				else
-					msg_string += "unknown";
-			}
-			// add to the list of strings to display
-			msg_strings[current_msg_string] = msg_string;
-			timers[current_msg_string] = ofGetElapsedTimef() + 5.0f;
-			current_msg_string = ( current_msg_string + 1 ) % NUM_MSG_STRINGS;
-			// clear the next line
-			msg_strings[current_msg_string] = "";
-		}
+        std::cout << m.getAddress();
+
+        for ( int i=0; i<m.getNumArgs(); i++ )
+        {
+            // get the argument type
+
+            // display the argument - make sure we get the right type
+            /*if( m.getArgType( i ) == OFXOSC_TYPE_INT32 )
+                msg_string += ofToString( m.getArgAsInt32( i ) );
+            else*/ if( m.getArgType( i ) == OFXOSC_TYPE_FLOAT )
+                std::cout << " " << ofToString( m.getArgAsFloat( i ) );
+             if( m.getArgType( i ) == OFXOSC_TYPE_STRING )
+                std::cout << " " << m.getArgAsString( i );
+            /*else
+                msg_string += "unknown";*/
+        }
+
+        std::cout << std::endl;
+
+        // add to the list of strings to display
+        //msg_strings[current_msg_string] = msg_string;
+        //timers[current_msg_string] = ofGetElapsedTimef() + 5.0f;
+        //current_msg_string = ( current_msg_string + 1 ) % NUM_MSG_STRINGS;
+        // clear the next line
+        //msg_strings[current_msg_string] = "";
+
 
 	}
 
