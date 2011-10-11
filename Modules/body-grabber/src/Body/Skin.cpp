f9b320f58517ec734f2559f7cb1d0eb1a26d1bbc
David Stolarsky <david.stolarsky@gmail.com>
2011-10-11 01:05:46 -0400

    messing with canonical transformation; no convincing results. moving on to surface reconstruction and export.

--- Modules/body-grabber/src/Body/Skin.cpp
+++ Modules/body-grabber/src/Body/Skin.cpp
@@ -58,18 +58,33 @@ void localize(float *pos, float *orientation, const pcl::PointXYZRGB *input, pcl
 	output->y = input->y - pos[1];
 	output->z = input->z - pos[2];
 	
-	// matrix multiply
+	// matrix multiply (original)
+
 	output->x = dot(&orientation[0], (float*)output);
-	output->y = dot(&orientation[3], (float*)output);
+	output->y = - dot(&orientation[3], (float*)output);
 	output->z = dot(&orientation[6], (float*)output);
+
+	// matrix multiply (trial-and-error row swaps)
+	//output->x = dot(&orientation[3], (float*)output);
+	//output->y = dot(&orientation[0], (float*)output);
+	//output->z = dot(&orientation[6], (float*)output);
 }
 
 void globalize(Body::Skeleton::JointPose *joint, float weight, pcl::PointXYZ *local, pcl::PointXYZRGB *output) {
 	float *position = (float*)&joint->position.position;
 	float *orientation = joint->orientation.orientation.elements;
+
+	// original
 	output->x += weight*( position[0]/1000. + dot(&orientation[0], (float*)local));
-	output->y += weight*(-position[1]/1000. + dot(&orientation[3], (float*)local));
+	output->y += weight*(-position[1]/1000. /*+*/ - dot(&orientation[3], (float*)local));
 	output->z += weight*( position[2]/1000. + dot(&orientation[6], (float*)local));
+
+	// trial-and-error row swaps
+	//float orientation_transpose[9];
+	//transpose(orientation, orientation_transpose);
+	//output->x += weight*( position[0]/1000. +       dot(&orientation_transpose[0], (float*)local));
+	//output->y += weight*(-position[1]/1000. + /*-*/ dot(&orientation_transpose[3], (float*)local));
+	//output->z += weight*( position[2]/1000. +       dot(&orientation_transpose[6], (float*)local));
 }
 
 void Skin::bind(pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr all_points, const Body::Skeleton::Pose::Ptr bind_pose) {
@@ -91,6 +106,7 @@ void Skin::bind(pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr all_points, const Bo
 		positions[j][0] = v.X/1000.; positions[j][1] = -v.Y/1000.; positions[j][2] = v.Z/1000.;
 		// inverse
 		transpose((float*)config.orientation.orientation.elements, orientations[j]);
+		//memcpy(orientations[j], config.orientation.orientation.elements, sizeof(config.orientation.orientation.elements));
 	}
 
 	newest_bone_clouds.clear();
