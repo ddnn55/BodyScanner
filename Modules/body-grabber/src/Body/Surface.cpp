39a2ff30bf3c78f7092ce0ea98ca15910376d201
Mark Luffel <markluffel@gmail.com>
2011-10-10 23:18:14 -0400

    working surface.cpp

--- Modules/body-grabber/src/Body/Surface.cpp
+++ Modules/body-grabber/src/Body/Surface.cpp
@@ -9,17 +9,18 @@
 
 namespace Body {
 
+
 void filterPoints(pcl::PointCloud<pcl::PointXYZRGB>::Ptr input_cloud,
 									pcl::PointCloud<pcl::PointXYZRGB>::Ptr filtered_cloud) {
 										
-		pcl::VoxelGrid<pcl::PointCloud<pcl::PointXYZRGB> > sor;
+		pcl::VoxelGrid<pcl::PointXYZRGB> sor;
 		sor.setInputCloud(input_cloud);
 		sor.setLeafSize(0.01f, 0.01f, 0.01f);
 		sor.filter(*filtered_cloud);
 }
 
 void smoothPoints(pcl::PointCloud<pcl::PointXYZRGB>::Ptr input_points,
-									pcl::PointCloud<pcl::PointNormal>::Ptr output_points) {
+									pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr output_points) {
 
   //pcl::PointCloud<pcl::PointXYZ>::Ptr no_color_cloud(new pcl::PointCloud<pcl::PointXYZ>());
 	//pcl::copyPointCloud(input_points);
@@ -40,17 +41,17 @@ void smoothPoints(pcl::PointCloud<pcl::PointXYZRGB>::Ptr input_points,
   mls.setSearchMethod(tree);
   mls.setSearchRadius(0.03);
 
-	pcl::PointCloud<pcl::PointXYZ>::Ptr mls_points(new pcl::PointCloud<pcl::PointXYZ>());
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr mls_points(new pcl::PointCloud<pcl::PointXYZRGB>());
   // Reconstruct
-  mls.reconstruct(mls_points);
+  mls.reconstruct(*mls_points);
 
-  pcl::concatenateFields(mls_points, *mls_normals, output_points)
+	pcl::concatenateFields(*mls_points, *mls_normals, *output_points);
 }
 
 
 pcl::PolygonMesh::Ptr buildSurface(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud) {
 	pcl::PointCloud<pcl::PointXYZRGB>::Ptr filtered_cloud(new pcl::PointCloud<pcl::PointXYZRGB>());
-	pcl::PointCloud<pcl::PointNormal>::Ptr smooth_cloud(new pcl::PointCloud<pcl::PointNormal>());
+	pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr smooth_cloud(new pcl::PointCloud<pcl::PointXYZRGBNormal>());
 	printf("filtering points\n");
 	filterPoints(cloud, filtered_cloud); // this will reduce the # of points
 	printf("smoothing points\n");
@@ -60,12 +61,12 @@ pcl::PolygonMesh::Ptr buildSurface(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud)
 	
 	printf("constructing kdtree\n");
   // Create search tree*
-  pcl::KdTree<pcl::PointNormal>::Ptr tree(new pcl::KdTreeFLANN<pcl::PointNormal>);
+  pcl::KdTree<pcl::PointXYZRGBNormal>::Ptr tree(new pcl::KdTreeFLANN<pcl::PointXYZRGBNormal>);
   tree->setInputCloud(smooth_cloud);
 
   // Initialize objects
-  pcl::GreedyProjectionTriangulation<pcl::PointNormal> gp3;
-  pcl::PolygonMesh triangles;
+  pcl::GreedyProjectionTriangulation<pcl::PointXYZRGBNormal> gp3;
+  pcl::PolygonMesh::Ptr triangles(new pcl::PolygonMesh());
 
   // Set the maximum distance between connected points (maximum edge length)
   gp3.setSearchRadius(0.1);
@@ -82,10 +83,10 @@ pcl::PolygonMesh::Ptr buildSurface(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud)
   gp3.setInputCloud(smooth_cloud);
   gp3.setSearchMethod(tree);
 	printf("constructing surface\n");
-  gp3.reconstruct(triangles);
+  gp3.reconstruct(*triangles);
    
 	// copy colors back onto surface
-	pcl::copyPointCloud(*filtered_cloud, triangles.points);
+	//pcl::copyPointCloud(*filtered_cloud, triangles->cloud);
 	return triangles;
 }
 
